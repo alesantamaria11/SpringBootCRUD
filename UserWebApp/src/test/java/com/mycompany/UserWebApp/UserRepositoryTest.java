89fcba9179f2f845eddc4cfd69c2f9a39689bea7
alesantamaria11 <alesantamariadh@gmail.com>
2023-02-03 19:39:41 -0300

    testAddNew corrido con éxito

--- UserWebApp/src/test/java/com/mycompany/UserWebApp/UserRepositoryTest.java
+++ UserWebApp/src/test/java/com/mycompany/UserWebApp/UserRepositoryTest.java
@@ -1,4 +1,9 @@
 package com.mycompany.UserWebApp;
+import com.mycompany.UserWebApp.user.User;
+import com.mycompany.UserWebApp.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
@@ -7,5 +12,22 @@ import org.springframework.test.annotation.Rollback;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 public class UserRepositoryTest {
+
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setEmail("ale.san@gmail.com");
+        user.setPassword("ale1234");
+        user.setFirstName("Ale");
+        user.setLastName("Santamaria");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+
+    }
 
 }
